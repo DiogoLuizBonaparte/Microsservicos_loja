c1c8546c8b23b806981f188ea92f6b581d3cf729
Bonaparte <diogo322@hotmail.com>
2024-01-16 22:54:18 -0300

    front

--- FrontGeek/obj/Debug/net7.0/FrontGeek.AssemblyInfo.cs
+++ FrontGeek/obj/Debug/net7.0/FrontGeek.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Geek_Product")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("FrontGeek")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aced8c155ced23d9c03500982a77fc4f3d46e7c4")]
-[assembly: System.Reflection.AssemblyProductAttribute("Geek_Product")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Geek_Product")]
+[assembly: System.Reflection.AssemblyProductAttribute("FrontGeek")]
+[assembly: System.Reflection.AssemblyTitleAttribute("FrontGeek")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
