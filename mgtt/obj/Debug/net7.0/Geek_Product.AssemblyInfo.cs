c03df85a45ebf2e8fbb20ee71e5bb8ce755cded1
Bonaparte <diogo322@hotmail.com>
2024-01-17 23:27:38 -0300

    Dependeces

--- mgtt/obj/Debug/net7.0/Geek_Product.AssemblyInfo.cs
+++ mgtt/obj/Debug/net7.0/Geek_Product.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Geek_Product")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aced8c155ced23d9c03500982a77fc4f3d46e7c4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1c8546c8b23b806981f188ea92f6b581d3cf729")]
 [assembly: System.Reflection.AssemblyProductAttribute("Geek_Product")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Geek_Product")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
